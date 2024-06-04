902deb527f91a6a73dba1c462394a630ee30d3d5
officialpryz <afolabipraise43@gmail.com>
2024-06-04 09:09:13 -0700

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,7 +2,6 @@
 """Contains recurse function"""
 import requests
 
-
 def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
